5f100310d8a9304bd2c336a24d3e93372d5cc93e
Jaroslav Mach <jarda.mach@gmail.com>
2015-10-11 21:51:27 +0200

    Clean project

---
+++ Playground/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Playground
+//
+//  Created by Jaroslav Mach on 11/10/15.
+//  Copyright © 2015 Jaroslav Mach. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ Podfile
@@ -0,0 +1,5 @@
+source 'https://github.com/CocoaPods/Specs.git'
+platform :ios, '8.0'
+
+# ignore all warnings from all pods
+inhibit_all_warnings!
